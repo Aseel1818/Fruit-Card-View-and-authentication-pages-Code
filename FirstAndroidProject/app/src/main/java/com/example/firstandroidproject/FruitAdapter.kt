de71d946f3c3bd19651e3d0cb9c47b950cf725ea
Aseel Hamayel <63041611+Aseel1818@users.noreply.github.com>
2025-04-18 23:24:01 +0300

    change id
    change id

--- FirstAndroidProject/app/src/main/java/com/example/firstandroidproject/FruitAdapter.kt
+++ FirstAndroidProject/app/src/main/java/com/example/firstandroidproject/FruitAdapter.kt
@@ -8,7 +8,6 @@ import android.widget.Filter
 import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
 class FruitAdapter(private val context: Context, private var fruitList: List<Fruit>) :
